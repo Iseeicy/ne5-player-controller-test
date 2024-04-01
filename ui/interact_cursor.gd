50e2c6d1bbdd589e2fbfe7d2e23aaf1f4184777c
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-01 14:24:40 -0500

    Uses new interactables

--- ui/interact_cursor.gd
+++ ui/interact_cursor.gd
@@ -9,13 +9,12 @@ func _ready():
 	set_over_interactable(false)
 
 	var player = PlayerController.find(get_tree().root)
-	await player.ready
-	player.interactor.interact_hover_changed.connect(set_over_interactable.bind())
+	if not player.is_node_ready(): await player.ready
+	player.interactor.is_hovering_changed.connect(set_over_interactable.bind())
 
 func set_over_interactable(is_over_interactable: bool):
-	if is_over_interactable == over_interactable:
-		return
-	
+	if is_over_interactable == over_interactable: return
+
 	over_interactable = is_over_interactable
 	color.a = 1.0 if over_interactable else 0.2
 	
